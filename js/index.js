fe5d679c10ec6f3539f9feb7fb4421db1a31a945
Lucie Yarish <lucieyarish@hotmail.com>
2024-01-03 07:40:47 +0100

    Finish header

--- js/index.js
+++ js/index.js
@@ -4,6 +4,13 @@ const header = document.getElementById('header');
 
 const renderHeader = () => {
   const headerHtml = `
-        <
+        <div class="header-background">
+            <h1 class="title">Delish donuts</h1>
+            <h3 class="subtitle">The best donuts in town.</h3>
+        </div>
     `;
+
+  header.innerHTML = headerHtml;
 };
+
+renderHeader();
