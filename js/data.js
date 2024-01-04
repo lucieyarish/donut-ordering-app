75afdb894fd5e62fff4c86df8ddcccc69cd86ea1
Lucie Yarish <lucieyarish@hotmail.com>
2024-01-04 07:29:52 +0100

    Render menu items; add all styles to menu; add alt text and image to all data items

--- js/data.js
+++ js/data.js
@@ -1,35 +1,49 @@
 export const menu = [
   {
     uuid: 'f71b2cc0-e12f-4ad2-83f4-46eb5c02a58c',
+    image: 'assets/images/strawberry-donut.png',
     name: 'Strawberry Milkshake Donut',
+    altText: 'Digital art donut with pink frosting and strawberries on top',
     ingredients: ['fresh strawberries', 'strawberry buttercream frosting'],
     price: 7,
     isVegan: false,
   },
   {
     uuid: 'c27a2b16-da1e-4f52-b868-abf2c6720fa9',
+    image: 'assets/images/caramel-peanut-donut.png',
     name: 'Peanut Caramel Donut',
+    altText:
+      'Digital art donut with light brown frosting and chopped peanuts on top surrounded by many tiny donuts with no frosting',
     ingredients: ['peanuts', 'vegan caramel frosting'],
     price: 7,
     isVegan: true,
   },
   {
     uuid: 'f1f4ab39-ce2b-4ccb-816b-baaa1a79ab86',
+    image: 'assets/images/blueberry-donut.png',
     name: 'Blueberry Donut',
+    altText:
+      'Digital art donut with blue frosting, blueberries and petals on top, standing on its side at a beach',
     ingredients: ['fresh blueberries', 'vegan blueberry frosting'],
     price: 7,
     isVegan: true,
   },
   {
     uuid: '516ba349-3ec0-463d-b831-c2e3425d10ab',
+    image: 'assets/images/cappuccino-cup.png',
     name: 'Cappuccino',
+    altText:
+      'Digital art cup of cappuccino with beautiful latte art standing on a wooden table with exotic flowers in the background',
     ingredients: ['espresso', 'steamed milk', 'foam'],
     price: 5,
     isVegan: false,
   },
   {
     uuid: '4fb25831-6d9e-4aa0-9f29-56da3d14be63',
+    image: 'assets/images/oat-milk.png',
     name: 'Oat Milk',
+    altText:
+      'Digital art box of oat milk and a glass full of it in the foreground standing on a kitchen counter',
     ingredients: ['plant-based oat milk for your coffee'],
     price: 0,
     isVegan: true,
--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
 import { menu } from './data.js';
 
 const header = document.getElementById('header');
+const menuContainer = document.getElementById('menu-container');
 
 const renderHeader = () => {
   const headerHtml = `
         <div class="header-background">
-            <h1 class="title">Delish donuts</h1>
-            <h3 class="subtitle">The best donuts in town.</h3>
+            <h1 class="logo-title">Delish donuts</h1>
+            <h3 class="logo-subtitle">The best donuts in town.</h3>
         </div>
     `;
 
@@ -14,3 +15,27 @@ const renderHeader = () => {
 };
 
 renderHeader();
+
+const renderMenu = () => {
+  const menuHtml = menu
+    .map((item) => {
+      return `
+        <li>
+            <div class="menu-item-container">
+                <img src="${item.image}" class="menu-item-img"alt="${item.altText}">
+                <div class="menu-item-description">
+                    <h3 class="item-name">${item.name}</h3>
+                    <p class="item-ingredients">${item.ingredients}</p>
+                    <h4 class="item-price">$${item.price}</h4>
+                </div>
+                <button class="btn-add">+</button>
+            </div>
+        </li>
+    `;
+    })
+    .join('');
+
+  menuContainer.innerHTML += menuHtml;
+};
+
+renderMenu();
