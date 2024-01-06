c4599b937eae2fa2197a9e430a7a7721e2b865f7
Lucie Yarish <lucieyarish@hotmail.com>
2024-01-06 08:56:47 +0100

    Feature: render cart items with remove btn

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 import { menu } from './data.js';
 
 const header = document.getElementById('header');
-const menuContainer = document.getElementById('menu-container');
-const checkoutBtn = document.getElementById('btn-checkout');
+const mainContainer = document.getElementById('main-container');
+
+const badgePath = '../assets/images/vegan-badge.png';
+const veganBadge = `<img src="${badgePath}" class="badge-img">`;
 
 const cartItems = [];
 
@@ -20,8 +22,6 @@ const renderHeader = () => {
 renderHeader();
 
 const renderMenu = () => {
-  const badgePath = '../assets/images/vegan-badge.png';
-  const veganBadge = `<img src="${badgePath}" class="badge-img">`;
   const menuHtml = menu
     .map((item) => {
       const ingredients = item.ingredients.join(', ');
@@ -38,20 +38,26 @@ const renderMenu = () => {
                     <p class="item-ingredients">${ingredients}</p>
                     <h4 class="item-price">$${item.price}</h4>
                 </div>
-                <button id="${item.uuid}" class="btn-add">+</button>
+                <button id="${item.uuid}" class="btn-cart">+</button>
             </div>
         </li>
     `;
     })
     .join('');
 
-  menuContainer.innerHTML += menuHtml;
+  mainContainer.innerHTML += menuHtml;
+  mainContainer.innerHTML += `
+    <btn id="btn-checkout" class="btn-primary btn-disabled" disabled>
+        Checkout
+    </btn>
+`;
 };
 
 renderMenu();
 
-menuContainer.addEventListener('click', function (e) {
+mainContainer.addEventListener('click', function (e) {
   if (e.target.id) {
+    const checkoutBtn = document.getElementById('btn-checkout');
     const itemToAdd = menu.find((i) => i.uuid === e.target.id);
     cartItems.push(itemToAdd);
     //TODO: only perform these 3 steps after amount of cartItems changes from 0 to 1
@@ -59,4 +65,37 @@ menuContainer.addEventListener('click', function (e) {
     checkoutBtn.classList.remove('btn-disabled');
     checkoutBtn.classList.add('btn-active');
   }
+});
+
+const renderOrder = () => {
+  const orderHtml = cartItems
+    .map((item) => {
+      const ingredients = item.ingredients.join(', ');
+      return `
+        <li >
+            <div class="menu-item-container border-hidden">
+                <img 
+                    src="${item.image}" 
+                    class="menu-item-img"
+                    alt="${item.altText}">
+                <div class="menu-item-description">
+                    <h3 class="item-name">${item.name}
+                    <span>${item.isVegan ? veganBadge : ''}</span>
+                    </h3>
+                    <p class="item-ingredients">${ingredients}</p>
+                    <h4 class="item-price">$${item.price}</h4>
+                </div>
+                <button id="${item.uuid}" class="btn-cart btn-remove">-</button>
+            </div>
+        </li>
+    `;
+    })
+    .join('');
+
+  mainContainer.innerHTML += orderHtml;
+};
+
+document.getElementById('btn-checkout').addEventListener('click', function () {
+  mainContainer.innerHTML = '';
+  renderOrder();
 });
